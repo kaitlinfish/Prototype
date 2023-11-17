918527ff3c732811cea5034f24a6227fb42f2f11
kaitlinfish <knfish@hotmail.com>
2023-11-17 14:42:58 +0000

    initial-commit

---
+++ test/Tickets.js
@@ -0,0 +1,23 @@
+const Tickets = artifacts.require('Tickets');
+const assert = require('assert');
+
+contract('Tickets',(accounts)=> {
+    const BUYER = accounts[1];
+    const TICKET_ID = 0;
+
+    it('should allow a user to buy a ticket', async () => {
+        const instance = await Tickets.deployed();
+        const origionalTicket = await instance.tickets(
+            TICKET_ID
+        );
+       await instance.buyTicket(TICKET_ID, {
+        from:BUYER, 
+        value: origionalTicket.price, });
+
+     const updatedTicket = await instance.tickets(
+        TICKET_ID
+     );
+        assert.equal(updatedTicket.owner, BUYER, 'the buyer should now own this ticket'
+        );
+    });
+});
